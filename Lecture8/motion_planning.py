d81123e94a9664da061e96a04c7c67f74276d3da
OliviaWYQ <295199218@qq.com>
2026-07-14 15:23:36 +0800

    add Lecture8: motion planning code and homework

---
+++ Lecture8/motion_planning.py
@@ -0,0 +1,9 @@
+def get_path_from_A_star(start, goal, obstacles):
+    # input  start: integer 2-tuple of the current grid point, e.g., (0, 0)
+    #        goal: integer 2-tuple  of the goal grid point, e.g., (5, 1)
+    #        obstacles: a list of grid points marked as obstacles, e.g., [(2, -1), (2, 0), ...]
+    # output path: a list of grid points connecting start to goal, e.g., [(1, 0), (1, 1), ...]
+    #   note that the path should contain the goal but not the start
+    #   e.g., the path from (0, 0) to (2, 2) should be [(1, 0), (1, 1), (2, 1), (2, 2)]
+   
+    return path
